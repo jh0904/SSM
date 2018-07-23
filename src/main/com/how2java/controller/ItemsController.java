a37396419ca5f8aa36f8225a830e4901eb4c4026
jh0904 <464062919@qq.com>
2018-07-23 10:59:53 +0800

    添加了文件上传

--- src/main/com/how2java/controller/ItemsController.java
+++ src/main/com/how2java/controller/ItemsController.java
@@ -14,12 +14,15 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * com.how2java.controller
@@ -35,19 +38,17 @@ public class ItemsController {
     @Autowired
     ItemsService itemsService;
 
-
     // 商品分类
     //itemtypes表示最终将方法返回值放在request中的key
     @ModelAttribute("itemtypes")
     public Map<String, String> getItemTypes() {
 
         Map<String, String> itemTypes = new HashMap<String, String> ();
-        itemTypes.put("101", "数码");
-        itemTypes.put("102", "母婴");
+        itemTypes.put ("101", "数码");
+        itemTypes.put ("102", "母婴");
 
         return itemTypes;
     }
-
 
     @RequestMapping("queryItems")
     public ModelAndView queryItems(HttpServletRequest request, ItemsQueryVo itemsQueryVo) throws Exception {
@@ -103,10 +104,12 @@ public class ItemsController {
      * @Validated(value={ValidGroup1.class})分组校验
      */
     @RequestMapping("editItemsSubmit")
-    public String editItemsSubmit(HttpServletRequest request, Model model, Integer id, @ModelAttribute("items") @Validated(value = {ValidGroup1.class}) ItemsCustom itemsCustom, BindingResult bindingResult) throws Exception {
+    public String editItemsSubmit(HttpServletRequest request, Model model, Integer id, @ModelAttribute("items") @Validated(value = {ValidGroup1.class}) ItemsCustom itemsCustom, BindingResult bindingResult, MultipartFile items_pic) throws Exception {
         /*ModelAndView modelAndView = new ModelAndView ();
         modelAndView.setViewName ("success");*/
         //        //return "redirect:queryItems";
+
+        System.out.println ("---------------------->");
         if (bindingResult.hasErrors ()) {
             List<ObjectError> allErrors = bindingResult.getAllErrors ();
 
@@ -116,14 +119,36 @@ public class ItemsController {
 
             }
             model.addAttribute ("allErrors", allErrors);
-            model.addAttribute ("items",itemsCustom);
+            model.addAttribute ("items", itemsCustom);
             return "editItems";
+        }
+        //原始名称
+        String originalFilename = items_pic.getOriginalFilename();
+        //上传图片
+        if(items_pic!=null && originalFilename!=null && originalFilename.length ()>0){
+
+            //存储图片的物理路径
+            String pic_path = "D:\\java\\photo\\";
+
+
+            //新的图片名称
+            String newFileName = UUID.randomUUID() + originalFilename.substring(originalFilename.lastIndexOf("."));
+            //新图片
+
+            File newFile = new File(pic_path+newFileName);
+
+            //将内存中的数据写入磁盘
+            items_pic.transferTo(newFile);
+
+            //将新图片名称写到itemsCustom中
+            itemsCustom.setPic(newFileName);
+
         }
 
         System.out.println ("---------------------->" + itemsCustom.getCreatetime ());
         itemsService.updateItems (id, itemsCustom);
-        //return "forward:queryItems";
-        return "queryItems";
+        return "forward:queryItems";
+        //return "queryItems";
     }
 
     /**
